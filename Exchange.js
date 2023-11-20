012d3042b33e4584f8fd38790152c5e8903155eb
Lethabo <paulchigo22@gmail.com>
2023-11-20 20:19:41 +0200

    Remove unnecessary code and update TradingView widget

--- Exchange.js
+++ Exchange.js
@@ -1,34 +1,16 @@
-const express = require("express");
-const axios = require("axios");
-
-const app = express();
-
-app.get("/chart-data", async (req, res) => {
-  try {
-    const response = await axios.get("https://api.binance.com/api/v3/klines", {
-      params: {
-        symbol: "BTCUSDT",
-        interval: "1d",
-        limit: 365,
-      },
-    });
-
-    const chartData = response.data.map((candle) => ({
-      date: new Date(candle[0]).toISOString(),
-      open: parseFloat(candle[1]),
-      high: parseFloat(candle[2]),
-      low: parseFloat(candle[3]),
-      close: parseFloat(candle[4]),
-      volume: parseFloat(candle[5]),
-    }));
-
-    res.json(chartData);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal Server Error");
-  }
-});
-
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+// Your JavaScript code goes here
+document.addEventListener("DOMContentLoaded", function () {
+  // TradingView widget initialization code goes here
+  new TradingView.widget({
+    autosize: true,
+    symbol: "BINANCE:BTCUSDT",
+    interval: "D",
+    timezone: "Etc/UTC",
+    theme: "light",
+    style: "1",
+    locale: "en",
+    enable_publishing: false,
+    allow_symbol_change: true,
+    container_id: "tradingview_92fb7",
+  });
 });
