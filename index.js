6879d90d1400492eee8ce838cc67714240dae8f0
Lethabo <paulchigo22@gmail.com>
2023-12-11 14:08:42 +0200

    Refactor dark mode functionality and set it as the default theme

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const switchElement = document.getElementById("switch-1");
-switchElement.addEventListener("change", function () {
-  const isDarkMode = switchElement.checked;
-  localStorage.setItem("darkMode", isDarkMode);
-});
+
 // Function to toggle dark mode
 function toggleDarkMode() {
   const body = document.body;
   body.classList.toggle("dark-mode");
-}
-
-function setDefaultDarkMode() {
-  const body = document.body;
-  body.classList.add("dark-mode");
 
   // Save the user's preference in local storage
   const isDarkMode = body.classList.contains("dark-mode");
   localStorage.setItem("darkMode", isDarkMode);
+}
+
+// Function to set dark mode as the default theme
+function setDefaultDarkMode() {
+  const body = document.body;
+  const switchElement = document.getElementById("switch-1");
+
+  // Add dark mode class to body
+  body.classList.add("dark-mode");
+
+  // Set switch element to checked
+  switchElement.checked = true;
+
+  // Save the user's preference in local storage
+  localStorage.setItem("darkMode", true);
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -25,10 +32,13 @@ document.addEventListener("DOMContentLoaded", function () {
     "(prefers-color-scheme: dark)"
   ).matches;
 
-  switchElement.addEventListener("change", toggleDarkMode);
-
-  // Set default mode to dark mode
+  // Set dark mode as the default theme
   if (prefersDarkMode) {
+    body.classList.add("dark-mode");
+    switchElement.checked = true;
+  } else {
     setDefaultDarkMode();
   }
+
+  switchElement.addEventListener("change", toggleDarkMode);
 });
